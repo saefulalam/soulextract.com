09870928cf12f17480a51939181e65e0356df761
saefulalam <157026455+saefulalam@users.noreply.github.com>
2025-02-02 08:31:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,17 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
-const env = process.env.NODE_ENV || 'development';
-const port = process.env.PORT || 14000;
-const host = process.env.HOST || '0.0.0.0';
-
 const app = express();
 
+// Middleware
 app.use(helmet());
 app.use(morgan('common'));
 app.use(express.static('public'));
 
-app.listen(port, host, error => {
-  if (error) throw error;
-  console.log(`Server environment "${env}".`);
-  console.log(`Server listening at ${host}:${port}.`);
+// API Route
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Vercel!' });
 });
+
+// Export Express app sebagai handler untuk Serverless Functions
+module.exports = app;
